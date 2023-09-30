861e2811d43be261ea0a97c5e80f516841bf791e
gongahkia <angryapplegravy@gmail.com>
2023-10-01 00:52:49 +0800

    ok

--- src/interpreter.py
+++ src/interpreter.py
@@ -3,6 +3,7 @@
 # FUA: 
 # - write python code that iterates through the list and replace each word if necessary in groff / markdown / latex
 # - interpreter will need to actively handle the count of each kind of stylised word since we are not distinguishing between left and right for any styling option
+# - eventually incorporate a bash script that automatically calls the ooe interpreter with the 'ooe{version_number}' command called in terminal
 
 def print_token_array(token_array:list):
     print(token_array)
